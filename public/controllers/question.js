b5571edcbc6aff4c8e80c3fcf1ad8c11e0d563c2
Ariel Verber <opiedesigns@gmail.com>
2015-07-19 15:29:58 +0300

    voting system working

--- public/controllers/question.js
+++ public/controllers/question.js
@@ -8,6 +8,8 @@ pollyApp.controller('questionCtrl', ['$scope', '$http', '$routeParams', function
     $scope.response= "";
     console.log($scope.question.title +" receieved");
     console.log($scope.question.questionType);
+    $scope.canVote = true;
+    $scope.answersVoted = [];
 
     $scope.isFreeQuestion = function(){
       return $scope.question.questionType === "free";
@@ -23,7 +25,36 @@ pollyApp.controller('questionCtrl', ['$scope', '$http', '$routeParams', function
 
     $scope.addResponse = function(){
       $scope.question.responses.push($scope.response);
+      $http.put('/questions/ ', $scope.question);
       console.log($scope.question.responses);
+    };
+
+    $scope.addVote = function(index){
+      if($scope.question.questionType === "single"){
+        console.log("adding vote to answer: " +index);
+        if($scope.canVote === true){
+          canVote = false;
+          addVoteInServer(index, 1);
+        }
+      }
+      else if($scope.question.questionType === "multiple"){
+        if($scope.answersVoted.indexOf(index) > -1){
+          console.log("already voted..." +index +" to voted.. removing");
+          $scope.answersVoted.splice(index, 1);
+          addVoteInServer(index, -1);
+        }
+        else{
+          console.log("adding" +index +" to voted");
+          $scope.answersVoted.push(index);
+          addVoteInServer(index, 1);
+        }
+      }
+    };
+
+    addVoteInServer = function(index, numToAdd){
+      $http.put('/questions/vote', {question: $scope.question, response: index, numToAdd: numToAdd}).success(function(response) {
+      console.log(response);
+    });
     };
 
 
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/questions', function (req, res) {
 app.get('/questions/:id', function (req, res) {
   console.log("receieved request for: " +req.params.id);
   db.polly.findOne({"_id": ObjectId(req.params.id)}, function(err, docs){
-    console.log(docs);
+    //console.log(docs);
     res.json(docs);
   });
 });
@@ -32,6 +32,57 @@ app.post('/questions', function (req, res) {
     res.json(doc);
   });
 });
+
+app.put('/questions/vote', function (req, res) {
+  var id = req.body.question._id;
+  var index = req.body.response;
+  var numToAdd = req.body.numToAdd;
+  console.log("receieved request to add " +numToAdd + " to " +id +"in index " +index);
+  var answers = req.body.question.answers;
+  answers[index].votes++;
+  console.log(answers);
+
+
+  db.polly.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {answers: answers}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    });
+
+
+
+});
+
+
+app.put('/questions', function (req, res) {
+  console.log('-----------------');
+  var id = req.body._id;
+  var questionType = req.body.questionType;
+  console.log("called to edit " +id);
+  console.log("question type " +questionType);
+  console.log(req.body);
+  var responses = req.body.responses;
+
+  if(questionType === 'free'){
+    db.polly.findAndModify({
+      query: {_id: mongojs.ObjectId(id)},
+      update: {$set: {responses: req.body.responses, answers: {}}},
+      new: true}, function (err, doc) {
+        res.json(doc);
+      });
+  }
+  else{
+    db.polly.findAndModify({
+      query: {_id: mongojs.ObjectId(id)},
+      update: {$set: {answers: req.body.answers}},
+      new: true}, function (err, doc) {
+        res.json(doc);
+      }
+      );
+  }
+});
+
 
 
 app.listen(9000);
